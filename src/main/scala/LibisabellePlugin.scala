af45bcc94602d7df52d6f56f4c8d249e2ac1d8b8
Mikaël Mayer <mikael.mayer@epfl.ch>
2016-07-01 11:10:54 +0200

    Added the reason why copying. Do not check the names of top-most folders. Fixed a bug in the File's listing.

--- src/main/scala/LibisabellePlugin.scala
+++ src/main/scala/LibisabellePlugin.scala
@@ -21,22 +21,27 @@ object LibisabellePlugin extends AutoPlugin {
       val log = streams.log
       val target = rawTarget / "isabelle"
       if (source.exists()) {
-        def upToDate(in: File, out: File): Boolean = {
-          in.getName == out.getName && in.exists() && out.exists() && (
-          if(in.isDirectory && out.isDirectory) {
+        var reasonBecauseNotUpToDate = ""
+        implicit class B(b: Boolean) { @inline def -/>(msg: String): Boolean = { if(!b) reasonBecauseNotUpToDate = msg; b } }
+        def upToDate(in: File, out: File, testName: Boolean = true): Boolean = {
+          (!testName || (in.getName == out.getName) -/> s"$in != $out") &&
+          in.exists() -/> s"$in does not exist" &&
+          out.exists() -/>  s"$in does not exist" && (
+          if (in.isDirectory && out.isDirectory) {
             val inFiles = in.listFiles()
             val outFiles = out.listFiles()
-            inFiles.size == outFiles.size && inFiles.zip(outFiles).forall(in => upToDate(in._1, in._2))
+            (inFiles.size == outFiles.size)-/> s"$in has ${inFiles.size} files whereas $out has ${outFiles.size} files" &&
+            inFiles.zip(outFiles).forall(in => upToDate(in._1, in._2))
           } else if(in.isFile && out.isFile) {
-            in.lastModified() == out.lastModified()
-          } else false)
+            (in.lastModified == out.lastModified) -/> s"$out is more recent than $in"
+          } else false -/> s"$in and $out are not both directories or files")
         }
-        if(!upToDate(source, target / name)) {
-          log.info(s"Copying Isabelle sources from $source to $target")
+        if (!upToDate(source, target / name, testName = false)) {
+          log.info(s"Copying Isabelle sources from $source to $target because $reasonBecauseNotUpToDate")
           IO.delete(target)
           IO.copyDirectory(source, target / name, preserveLastModified=true)
         }
-        val files = (target ** "*").get.filter(_.isFile)
+        val files = ((target / name) ** "*").get.filter(_.isFile)
         val mapper = Path.rebase(target / name, "")
         val contents = files.map(mapper).map(_.get).mkString("\n")
         val list = target / ".libisabelle_files"
